b49550afba6a00d585cf785a94b3d1dbab80475f
npmbuilder143 <npmbuilder143@email.com>
2025-03-25 00:24:13 +0800

    BulkOrder result modal style fix

--- src/components/Main/BulkOrder/Modal.js
+++ src/components/Main/BulkOrder/Modal.js
@@ -1,13 +1,15 @@
 // src/components/Main/BulkOrder/Modal.js
 import { FaDownload, FaTimes } from 'react-icons/fa';
+import $GS from '../../../styles/constants';
+import Card from '../../Utils/Card';
 
 const Modal = ({ fileData, isVisible, onClose }) => {
   if (!isVisible) return null;
 
   return (
     <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-60 z-20">
-      <div className="bg-white p-6 rounded-lg max-w-md">
-        <h2 className="text-xl font-bold mb-4">Order Processed Successfully!</h2>
+      <Card>
+        <h2 className={`${$GS.textHeading_2}`}>Order Processed Successfully!</h2>
         <div className="flex flex-wrap gap-2">
           {fileData.pdfName && (
             <button
@@ -40,10 +42,10 @@ const Modal = ({ fileData, isVisible, onClose }) => {
             onClick={onClose}
             className="bg-gray-300 hover:bg-gray-400 text-black px-4 py-2 rounded-md"
           >
-            <FaTimes />
+            Close
           </button>
         </div>
-      </div>
+      </Card>
     </div>
   );
 };
